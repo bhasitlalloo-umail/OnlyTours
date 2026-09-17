5543adb707d7aaad9ac0a5db327596295fe71bc4
bhasitlalloo-umail <bhasit.lalloo@umail.uom.ac.mu>
2026-09-17 22:45:12 +0400

    Deleted incorrect files

--- OnlyTours/OnlyToursApp/urls.py
+++ OnlyTours/OnlyToursApp/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('admin/',views.admin, name='admin'),
     ]
--- OnlyTours/OnlyToursApp/views.py
+++ OnlyTours/OnlyToursApp/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 
 def register(request):
 	return render(request,'register.html')
-
-def admin(request):
-	return render(request,'admin.html')
