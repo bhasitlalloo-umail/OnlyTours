3db821cac682bc947f94b30103b7aec07defecf9
bhasitlalloo-umail <bhasit.lalloo@umail.uom.ac.mu>
2026-09-17 21:43:20 +0400

    Added admin to views and urls

--- OnlyTours/OnlyToursApp/urls.py
+++ OnlyTours/OnlyToursApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('admin/',views.admin, name='admin'),
     ]
--- OnlyTours/OnlyToursApp/views.py
+++ OnlyTours/OnlyToursApp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def register(request):
 	return render(request,'register.html')
+
+def admin(request):
+	return render(request,'admin.html')
